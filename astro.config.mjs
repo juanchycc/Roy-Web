187de9cd83c04f3d912e309f02d01286af18c5ac
juanchycc <jcaserio@fi.uba.ar>
2025-03-30 19:10:29 -0300

    deploy base web version

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://juanchycc.github.io',
+  base: '/Roy-Web',
   vite: {
     plugins: [tailwindcss()]
   }
