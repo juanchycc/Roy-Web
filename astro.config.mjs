089f98f6729736e8b368ed6caf746ed8965a39b9
juanchycc <jcaserio@fi.uba.ar>
2025-03-31 23:15:49 -0300

    change mobile/desktop and new header style

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://juanchycc.github.io',
-  base: '/Roy-Web',
   vite: {
     plugins: [tailwindcss()]
   }
