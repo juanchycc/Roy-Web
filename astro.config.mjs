49064703ec36575968b6f7a1d228bfe9af7f24cc
juanchycc <jcaserio@fi.uba.ar>
2025-04-01 21:21:36 -0300

    change base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://juanchycc.github.io',
+  base: '/Roy-Web',
   vite: {
     plugins: [tailwindcss()]
   }
